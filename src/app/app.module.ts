189dab9b864abff8e87d58ce85f074b22fc2036b
hcalderon <hendrick.calderon@hotmail.com>
2020-02-17 19:21:36 -0600

    Estructura inicial del proyecto arboleda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { PieComponent } from './pie/pie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    PieComponent
   ],
   imports: [
     BrowserModule,
